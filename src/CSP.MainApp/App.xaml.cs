9833ae5f60bf16a6c49cfc5705cc373d398ccda8
Adrian Kuczyński <adrian.kuczynski@globallogic.com>
2022-12-23 10:51:38 +0100

    init

---
+++ src/CSP.MainApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CSP.MainApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/CSP.MainApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CSP.MainApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
