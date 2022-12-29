f27fd65c0e381537b0fbbd4756c8cd1bdc98ea44
Adrian Kuczyński <adrian.kuczynski@globallogic.com>
2022-12-29 12:19:07 +0100

    add js-net example

---
+++ src/CSP.MainApp/Javascript/HelloHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSP.MainApp.Javascript
+{
+	// example was taken from here: https://learn.microsoft.com/en-us/aspnet/core/blazor/javascript-interoperability/call-dotnet-from-javascript?view=aspnetcore-7.0#class-instance-examples
+	public class HelloHelper
+	{
+		public HelloHelper(string? name)
+		{
+			Name = name ?? "No Name";
+		}
+
+		public string? Name { get; set; }
+
+		[JSInvokable]
+		public string GetHelloMessage() => $"Hello, {Name}!";
+
+	}
+}
---
+++ src/CSP.MainApp/Javascript/JsInteropClasses3.cs
@@ -0,0 +1,18 @@
+﻿using CSP.MainApp.Javascript;
+using Microsoft.JSInterop;
+
+public class JsInteropClasses3
+{
+	private readonly IJSRuntime js;
+
+	public JsInteropClasses3(IJSRuntime js)
+	{
+		this.js = js;
+	}
+
+	public async ValueTask<string> CallHelloHelperGetHelloMessage(string? name)
+	{
+		using var objRef = DotNetObjectReference.Create(new HelloHelper(name));
+		return await js.InvokeAsync<string>("sayHello1", objRef);
+	}
+}
