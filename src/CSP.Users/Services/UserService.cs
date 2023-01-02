007fd0258e71b2dd2814ce158bc45c2d06e9731e
Adrian Kuczyński <adrian.kuczynski@globallogic.com>
2023-01-02 08:36:08 +0100

    fixes

--- src/CSP.Users/Services/UserService.cs
+++ src/CSP.Users/Services/UserService.cs
@@ -23,39 +23,49 @@ namespace CSP.Users.Services
 		[ResourceMethod]
 		public IReadOnlyList<User> GetUsers(int page, int pageSize)
         {
-			var result = _database.GetAllAsync().Result;
-            return result; 
+			var task = _database.GetAllAsync();
+            task.Wait();
+
+            return task.Result; 
         }
 
 		// GET http://localhost:8080/users/:id/
 		[ResourceMethod(":id")]
         public User? GetUser(long id)
         {
-			var result = _database.GetByIdAsync(id).Result;
-			return result;
+			var task = _database.GetByIdAsync(id);
+            task.Wait();
+            return task.Result;
 		}
 
         [ResourceMethod(RequestMethod.PUT)]
         public long AddUser(string firstName, string lastName)
         {
             var user = new User { FirstName = firstName, LastName = lastName };
-            return _database.SaveItemAsync(user).Result;
+            var task = _database.SaveItemAsync(user);
+
+            task.Wait();
+            return task.Result;
         }
 
 		// POST http://localhost:8080/users/
 		[ResourceMethod(RequestMethod.POST)]
-        public async Task<long> UpdateUser(User user)
+        public long UpdateUser(User user)
         {
-			return await _database.SaveItemAsync(user);
-		}
+			var task = _database.SaveItemAsync(user);
+            task.Wait();
+
+            return task.Result;
+        }
 
         [ResourceMethod(RequestMethod.DELETE, ":id")]
-        public async void DeleteUser(long id)
+        public void DeleteUser(long id)
         {
-            var user = await _database.GetByIdAsync(id);
-            if (user != null)
+            var user = _database.GetByIdAsync(id);
+            if (user.Result != null)
             {
-                await _database.DeleteAsync(user);
+                var task = _database.DeleteAsync(user.Result);
+                task.Wait();
             }
         }
     }
