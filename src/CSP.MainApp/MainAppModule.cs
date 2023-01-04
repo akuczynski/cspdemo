9dd07198f9e96227add1a5a8e2582d8b6054a0e9
Adrian Kuczyński <adrian.kuczynski@globallogic.com>
2023-01-04 12:11:32 +0100

    clean project

--- src/CSP.MainApp/MainAppModule.cs
+++ src/CSP.MainApp/MainAppModule.cs
@@ -1,8 +1,5 @@
 ﻿using CSP.Books;
 using CSP.EntityFrameworkCore;
-using CSP.MobileWebGate;
-using CSP.Users;
-using CSP.WebGate;
 using Volo.Abp.Auditing;
 using Volo.Abp.Autofac;
 using Volo.Abp.Modularity;
@@ -10,20 +7,13 @@ using Volo.Abp.Modularity;
 namespace CSP.MainApp;
 
 [DependsOn(typeof(AbpAutofacModule),
-		   typeof(WebGateModule),
 		   typeof(CSPEntityFrameworkCoreModule),
-		   typeof(MobileWebGateModule),
-		   typeof(UsersModule), 
 		   typeof(BookModule)
 	)]
 public class MainAppModule : AbpModule
 {
 	public override void ConfigureServices(ServiceConfigurationContext context)
 	{
-		//Configure<AbpAutoMapperOptions>(options =>
-		//{
-		//	options.AddMaps<BookModule>();
-		//});
 		Configure<AbpAuditingOptions>(options =>
 		{
 			options.IsEnabled = false; //Disables the auditing system
