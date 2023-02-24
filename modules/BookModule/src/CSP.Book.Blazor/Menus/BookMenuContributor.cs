fe5677b45f1dfa9cf22506027928602b3e155f76
Adrian Kuczyński <adrian.kuczynski@globallogic.com>
2023-02-24 13:13:57 +0100

    update demo

--- modules/BookModule/src/CSP.Book.Blazor/Menus/BookMenuContributor.cs
+++ modules/BookModule/src/CSP.Book.Blazor/Menus/BookMenuContributor.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Volo.Abp.Authorization.Permissions;
 using Volo.Abp.UI.Navigation;
 
 namespace CSP.Book.Blazor.Menus;
@@ -16,8 +18,32 @@ public class BookMenuContributor : IMenuContributor
     private Task ConfigureMainMenuAsync(MenuConfigurationContext context)
     {
         //Add main menu items.
-        context.Menu.AddItem(new ApplicationMenuItem(BookMenus.Prefix, displayName: "Book", "/Book", icon: "fa fa-globe"));
+        var bookMenuItem = new ApplicationMenuItem(BookMenus.Prefix, displayName: "Book", "/Book", icon: "fa fa-globe");
 
+        //  when this menu item have to be visible only by the authenticated users then add call to RequireAuthenticated(); 
+        context.Menu.AddItem(bookMenuItem);
+
+        bookMenuItem.AddItem(new ApplicationMenuItem(
+                "BookSubPage",
+                "SubPage",
+                url: "~/Book/SubPage"
+            ).RequirePermissions("Book_Get_Author"));
+
+
+        bookMenuItem.AddItem(new ApplicationMenuItem(
+                "BookSubPage2",
+                "SubPage2",
+                url: "~/Book/SubPage2"
+            ).RequirePermissions("Book_Get_Quote"));
+         
         return Task.CompletedTask;
+    }
+
+
+    public Task<List<string>> GetList()
+    {
+        var list = new List<string>();
+
+        return Task.FromResult(list); 
     }
 }
